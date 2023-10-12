a330b0d43ec18bbca9512de521356e7c01b2a0ee
Hala Almohammadi <hala.almohammadi@kcl.ac.uk>
2023-10-12 03:42:48 +0100

    Add Things Form and display form in the home view

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+"""from django.test import TestCase
+from things.forms import ThingForm 
+from things.models import Thing 
+""
+class thingFormTestCase(TestCase):
+    #Unit tests of the Thing form.
 
-# Create your tests here.
+    def setUp(self):
+        self.form_input = {
+            'name': 'iPhone 15', 
+            'description': 'one of the best latest iPhones', 
+            'quantity': '1'
+        }
+
+    def test_valid_thing_form(self):
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())"""""
--- things/views.py
+++ things/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .forms import ThingForm 
 
 def home(request):
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html')
+
+def sign_up(request):
+    form = ThingForm()
+    return render(request, 'sign_up.html', {'form': form})
